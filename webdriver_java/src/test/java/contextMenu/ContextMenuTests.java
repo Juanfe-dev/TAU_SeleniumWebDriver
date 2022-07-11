00dd4350a16a9173823ab9314ab480daa54de21d
JuanFe <juan.gomez164@udea.edu.co>
2022-07-11 06:44:07 -0500

    Ch7.3 Modals & TAREA

---
+++ webdriver_java/src/test/java/contextMenu/ContextMenuTests.java
@@ -0,0 +1,17 @@
+package contextMenu;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class ContextMenuTests extends BaseTests {
+
+    @Test
+    public void testContextMenu(){
+        var cMenuPage = homePage.clickContextMenu();
+        cMenuPage.focusBox();
+        assertEquals(cMenuPage.getContextMenuText(), "You selected a context menu");
+        cMenuPage.confirmContextMenuAlert();
+    }
+}
